4f6fa03c6453f062bf26696653aea9e413c6ee79
Marcos Vinícius Moreira da Silva <254775220+marcosmoreira-dev@users.noreply.github.com>
2026-04-11 11:17:12 -0300

    criando componente do ciclo

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import { Container } from './components/Container'
 import { Menu } from './components/Menu'
 import { Logo } from './components/Logo'
 import { CountDown } from './components/CountDown'
-import { DefaultInput } from './DefaultInput'
+import { DefaultInput } from './components/DefaultInput'
+import { Cycles } from './components/Cycles'
 
 export function App() {
     return (
@@ -38,8 +39,7 @@ export function App() {
           </div>
 
           <div className="formRow">
-            <p>Ciclos</p>
-            <p>0 0 0 0 0 0 0</p>
+            <Cycles />
           </div>
 
           <div className="formRow">
